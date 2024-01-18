d1a5d22c3715158dad0ef16ce0a61d843560ce77
AJAY RAZZ <149960524+Ajayrazz@users.noreply.github.com>
2024-01-18 19:55:26 +0530

    Program that sort an array in descending order by using bubble sort.

---
+++ DescendinOrderSortingUsingBubbleSort.java
@@ -0,0 +1,24 @@
+public class DescendinOrderSortingUsingBubbleSort {
+    public static void bubbleSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
